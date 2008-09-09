e43439224efb30d94e20f5151bfe1dd9d735fefa
Stephen <Stephen@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2008-09-09 16:36:48 +0000

    - adding beginnings of Starter Kit

---
+++ trunk/StarterKit/Website/Default.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+#if DEBUG
+		this.StyleImport.IsDebug = true;
+		this.ScriptInclude.IsDebug = true;
+#endif
+	}
+}
