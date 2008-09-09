abcb85c37e101325c0a6f77d50f2820b75c0ebdd
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-09 18:59:34 +0000

    - StarterKit updates

--- StarterKit/Website/Scripts/Example.js
+++ StarterKit/Website/Scripts/Example.js
@@ -1,1 +1,20 @@
-/* Example.js */
+/*global JsonFx */
+/*
+	Example.js
+
+	Example script for the Starter Kit
+*/
+
+/* enable valid CSS to target browsers without reverting to CSS hacks */
+JsonFx.UI.setCssUserAgent();
+
+/*
+	it is a best practice to not clutter the global namespace
+	creating top level objects which contain variables and functions
+	allows us to simulate namespaces
+*/
+
+/* namespace Example */
+if ("undefined" === typeof window.Example) {
+	window.Example = {};
+}
