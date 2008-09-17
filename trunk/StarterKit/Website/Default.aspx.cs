751d8da1aeea1db5d63fb4ac85aae07a9aeca43c
Stephen <Stephen@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2008-09-17 15:43:42 +0000

    - add Gzip to page output

--- trunk/StarterKit/Website/Default.aspx.cs
+++ trunk/StarterKit/Website/Default.aspx.cs
@@ -2,14 +2,17 @@ using System;
 
 public partial class _Default : System.Web.UI.Page
 {
-	protected override void OnPreRender(EventArgs e)
+	protected override void OnInit(EventArgs e)
 	{
-		base.OnPreRender(e);
+		base.OnInit(e);
 
 #if DEBUG
 		// Pretty-Print rendering helps when debugging through script
 		this.StyleImport.IsDebug = true;
 		this.ScriptInclude.IsDebug = true;
+#else
+		// improve the Yslow rating
+		JsonFx.Handlers.CompiledBuildResult.EnableStreamCompression(this.Context);
 #endif
 	}
 }
