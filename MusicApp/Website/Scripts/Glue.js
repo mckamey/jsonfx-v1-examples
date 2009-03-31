65fe98ab739e9c4e4de82b99aab9d32a74a8d60f
Stephen McKamey <mckamey@users.noreply.github.com>
2009-03-31 16:16:20 +0000

    - abstracting sort logic out into generic reusable class - fixing MemberRow styles - adding ext-link functionality

---
+++ MusicApp/Website/Scripts/Glue.js
@@ -0,0 +1,23 @@
+﻿/*global JsonFx, Music */
+
+/* enable valid CSS to target browsers without reverting to CSS hacks */
+JsonFx.UA.setCssUserAgent();
+
+/* setup Ajax loading indicator */
+JsonFx.IO.Service.prototype.onBeginRequest = function() {
+	// only show if request takes longer than 200ms
+	Music.Loading.show(200);
+};
+
+JsonFx.IO.Service.prototype.onEndRequest = function() {
+	Music.Loading.hide();
+};
+
+JsonFx.Bindings.add(
+	"a.ext-link",
+	function(elem) {
+		$(elem).click(function() {
+			window.open(this.href);
+			return false;
+		});
+	});
---
+++ MusicApp/Website/Scripts/Sort.js
@@ -0,0 +1,84 @@
+﻿/*global Music */
+
+/*
+	this is a generalized sort utility
+	it maintains the current data list, field, direction
+	it also generates closure callbacks for column headers
+*/
+
+/* Music namespace */
+if ("undefined" === typeof window.Music) {
+	window.Music = {};
+}
+
+/* Ctor */
+Music.Sort = function(/*array*/ list) {
+
+	// private fields
+	var /*string*/ field = null,
+		/*bool*/ desc = false;
+
+	// private list sorting methods
+	/*int*/ function compare(/*object*/ a, /*object*/ b) {
+		a = a[field];
+		b = b[field];
+
+		// send empty items to end
+		var aIsEmpty = ("undefined" === typeof a || a === null);
+		var bIsEmpty = ("undefined" === typeof b || b === null);
+		if (aIsEmpty) {
+			return bIsEmpty ? 0 : 1;
+		}
+		if (bIsEmpty) {
+			return aIsEmpty ? 0 : -1;
+		}
+
+		// convert arrays to something comparable
+		if (a instanceof Array) {
+			a = a.join('\n');
+		}
+		if (b instanceof Array) {
+			b = b.join('\n');
+		}
+
+		if (a < b) {
+			return -1;
+		}
+		if (a > b) {
+			return 1;
+		}
+		return 0;
+	}
+
+	// reverse is same logic negated
+	/*int*/ function reverse(/*object*/ a, /*object*/ b) {
+		return -compare(a, b);
+	}
+
+	/*array*/ function sort(/*string*/ field, /*bool*/ desc) {
+		if (!list || !field) {
+			return list;
+		}
+
+		return list.sort(desc ? reverse : compare);
+	}
+
+	/*array*/ this.getList = function() {
+		return list;
+	};
+
+	// generates a closure which will sort the data and update the current state
+	/*function*/ this.createColSort = function(/*string*/ fld) {
+		// this can be called repeatedly
+		return function() {
+			// update the internal sort state
+			if (field === fld) {
+				desc = !desc;
+			}
+			field = fld;
+
+			// sort in place affecting all other closures built from this object
+			sort(fld, desc);
+		};
+	};
+};
