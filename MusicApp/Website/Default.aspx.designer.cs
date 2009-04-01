3c34de3b3e949cac8abbcad3cbe7fea867b9dac7
Stephen McKamey <mckamey@users.noreply.github.com>
2009-04-01 15:50:49 +0000

    - renaming of files - wrapped artist grid and list with info panel - added list to test page - removed old Bands example

--- MusicApp/Website/Default.aspx.designer.cs
+++ MusicApp/Website/Default.aspx.designer.cs
@@ -30,5 +30,14 @@ namespace MusicApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::JsonFx.UI.Jbst.Control Grid;
+        
+        /// <summary>
+        /// List control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::JsonFx.UI.Jbst.Control List;
     }
 }
