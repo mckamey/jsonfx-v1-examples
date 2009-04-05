cf44c3dace41f60c366d9f7800232e70d36ceac6
smm <smm@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2009-04-05 23:50:20 +0000

    - stubbing out artist CRUD methods

--- trunk/MusicApp/Website/Default.aspx.cs
+++ trunk/MusicApp/Website/Default.aspx.cs
@@ -14,8 +14,8 @@ namespace MusicApp
 
 			MusicApp.Services.MusicService service = new MusicApp.Services.MusicService();
 
-			this.Grid.InlineData = service.GetArtist(5L);
-			this.List.InlineData = service.GetArtist(1L);
+			this.Grid.InlineData = service.GetMembers(5L);
+			this.List.InlineData = service.GetMembers(1L);
 			this.All.InlineData = service.GetArtists();
 		}
 	}
