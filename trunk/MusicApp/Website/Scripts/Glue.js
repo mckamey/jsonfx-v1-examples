2a39c02580e3c75725ff487fe3d6c4e250a65fc0
smm <smm@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2009-04-05 03:15:50 +0000

    - adding some standard fading jQuery extensions - applied fading to some swap outs

--- trunk/MusicApp/Website/Scripts/Glue.js
+++ trunk/MusicApp/Website/Scripts/Glue.js
@@ -1,7 +1,11 @@
-﻿/*global JsonFx, Music */
+﻿/*global JsonFx, jQuery, Music */
+
+/* CssUserAgent --------------------------------------------------------- */
 
 /* enable valid CSS to target browsers without reverting to CSS hacks */
 JsonFx.UA.setCssUserAgent();
+
+/* loading indicator --------------------------------------------------------- */
 
 /* setup Ajax loading indicator */
 JsonFx.IO.Service.prototype.onBeginRequest = function() {
@@ -13,6 +17,8 @@ JsonFx.IO.Service.prototype.onEndRequest = function() {
 	Music.Loading.hide();
 };
 
+/* behavior bindings --------------------------------------------------------- */
+
 JsonFx.Bindings.add(
 	"a.ext-link",
 	function(elem) {
@@ -21,3 +27,51 @@ JsonFx.Bindings.add(
 			return false;
 		});
 	});
+
+/* jQuery extensions --------------------------------------------------------- */
+
+jQuery.fn.replaceWithFade = function(/*DOM*/ elem, /*string|number*/ fade) {
+	if ("undefined" === typeof fade) {
+		fade = 300;
+	}
+
+	// show/hide affect CSS display, so using fadeTo
+	elem = $(elem).fadeTo(0, 0.0);
+	this.fadeOut(fade).replaceWith(
+		elem.fadeTo(fade, 1.0)
+	);
+};
+
+jQuery.fn.removeFade = function(/*string|number*/ fade) {
+	if ("undefined" === typeof fade) {
+		fade = 300;
+	}
+
+	this.fadeOut(fade, function() {
+		$(this).remove();
+	});
+};
+
+jQuery.fn.beforeFade = function(/*DOM*/ elem, /*string|number*/ fade) {
+	if ("undefined" === typeof fade) {
+		fade = 300;
+	}
+
+	$(this).before($(elem).fadeIn(fade));
+};
+
+jQuery.fn.prependFade = function(/*DOM*/ elem, /*string|number*/ fade) {
+	if ("undefined" === typeof fade) {
+		fade = 300;
+	}
+
+	$(this).prepend($(elem).fadeIn(fade));
+};
+
+jQuery.fn.appendFade = function(/*DOM*/ elem, /*string|number*/ fade) {
+	if ("undefined" === typeof fade) {
+		fade = 300;
+	}
+
+	$(this).append($(elem).fadeIn(fade));
+};
