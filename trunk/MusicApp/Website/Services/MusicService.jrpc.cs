b4175a716543b8d9e57a51081e86955e878db63a
smm <smm@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2009-04-05 07:29:38 +0000

    - data now properly roundtrips

--- trunk/MusicApp/Website/Services/MusicService.jrpc.cs
+++ trunk/MusicApp/Website/Services/MusicService.jrpc.cs
@@ -6,12 +6,12 @@ using MusicApp.Model;
 
 namespace MusicApp.Services
 {
-	[JsonService(Namespace = "Music", Name = "MusicService")]
+	[JsonService(Namespace="Music", Name="Service")]
 	public class MusicService
 	{
 		#region Service Methods
 
-		[JsonMethod(Name = "getArtist")]
+		[JsonMethod(Name="getArtist")]
 		public object GetArtist(long artistID)
 		{
 			MusicDataContext DB = new MusicDataContext();
@@ -52,6 +52,37 @@ namespace MusicApp.Services
 					Genres = genres,
 					Members = members
 				};
+		}
+
+		[JsonMethod("saveMember")]
+		public Member SaveMember(Member member)
+		{
+			if (member == null)
+			{
+				throw new ArgumentNullException("member", "Member was null.");
+			}
+
+			if (member.MemberID <= 0)
+			{
+				// TODO: create new member
+				member.MemberID = 987654321;
+			}
+			else
+			{
+				// TODO: update existing member
+			}
+			return member;
+		}
+
+		[JsonMethod("deleteMember")]
+		public bool DeleteMember(long memberID)
+		{
+			if (memberID <= 0)
+			{
+				throw new ArgumentOutOfRangeException("memberID", "Invalid MemberID.");
+			}
+
+			return true;
 		}
 
 		#endregion Service Methods
