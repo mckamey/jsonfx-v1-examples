448045b12f75cb74803db271b3eb7a8f4b5a8461
Stephen McKamey <mckamey@users.noreply.github.com>
2009-05-04 04:30:52 +0000

    - don't need template switching for edit-genres

--- MusicApp/Website/Scripts/ArtistEdit.js
+++ MusicApp/Website/Scripts/ArtistEdit.js
@@ -199,9 +199,8 @@ Music.ArtistEdit = {
 	},// closureDelete
 
 	/*	generates a closure which maintains a reference to
-		the originally bound artist and the target template
-		for attaching to edit-genre buttons */
-	closureEditGenres: function(/*JBST*/ template, /*Artist*/ artist) {
+		the originally bound artist for edit-genre buttons */
+	closureEditGenres: function(/*Artist*/ artist) {
 		return function() {
 
 			alert("TODO: Edit Genres for ArtistID="+artist.ArtistID);
