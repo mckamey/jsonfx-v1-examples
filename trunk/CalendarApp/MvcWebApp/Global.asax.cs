537424926bc9f67cf2ec62593fac202a531a37b1
smm <smm@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2009-05-09 17:36:36 +0000

    - initial seed of second example project - borrowing generic UI elements from first example project

---
+++ trunk/CalendarApp/MvcWebApp/Global.asax.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web;
+
+namespace MvcWebApp
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			//http://msdn.microsoft.com/en-us/magazine/2009.01.extremeaspnet.aspx
+
+			routes.MapRoute(
+				"Calendar.Year",
+				"{year}",
+				new
+				{
+					controller = "Calendar",
+					action = "Year",
+					year = -1,
+					month = -1,
+					day = -1
+				}
+			);
+
+			routes.MapRoute(
+				"Calendar.Month",
+				"{year}/{month}",
+				new
+				{
+					controller = "Calendar",
+					action = "Month",
+					year = -1,
+					month = -1,
+					day = -1
+				}
+			);
+
+			routes.MapRoute(
+				"Calendar.Day",
+				"{year}/{month}/{day}",
+				new
+				{
+					controller = "Calendar",
+					action = "Day",
+					year = -1,
+					month = -1,
+					day = -1
+				}
+			);
+		}
+
+		protected void Application_Start()
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		protected void Application_BeginRequest()
+		{
+			// establish the user's timezone for this request
+
+			HttpCookie cookie = this.Context.Request.Cookies.Get("tz");
+			TimeZoneInfo timezone;
+
+			int offset;
+			if (cookie == null || !Int32.TryParse(cookie.Value, out offset))
+			{
+				// use server time
+				timezone = TimeZoneInfo.Local;
+			}
+			else
+			{
+				// use browser time
+				timezone = TimeZoneInfo.CreateCustomTimeZone(
+					cookie.Value,
+					TimeSpan.FromMinutes(-offset),// reverse direction
+					cookie.Value,
+					cookie.Value);
+			}
+
+			this.Context.Items["TimeZone"] = timezone;
+		}
+	}
+}
