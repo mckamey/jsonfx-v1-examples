dd3a3e1f52979bc01bf87d02c7bbe30345103755
Stephen McKamey <mckamey@users.noreply.github.com>
2009-05-09 19:05:14 +0000

    - simplified and aggregated DateTime utility methods

--- CalendarApp/MvcWebApp/Controllers/CalendarController.cs
+++ CalendarApp/MvcWebApp/Controllers/CalendarController.cs
@@ -5,59 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 
+using MvcWebApp.Models;
+
 namespace MvcWebApp.Controllers
 {
     public class CalendarController : Controller
 	{
 		#region Controller Actions
 
-		public ActionResult Year(int year, int month, int day)
+		public ActionResult Year(int year)
         {
-			this.ViewData["UserDate"] = this.BuildDate(year, month, day);
+			this.ViewData["UserDate"] = TimeUtility.BuildDate(year, -1, -1);
 
 			return View();
         }
 
-		public ActionResult Month(int year, int month, int day)
+		public ActionResult Month(int year, int month)
 		{
-			this.ViewData["UserDate"] = this.BuildDate(year, month, day);
+			this.ViewData["UserDate"] = TimeUtility.BuildDate(year, month, -1);
 
 			return View();
 		}
 
 		public ActionResult Day(int year, int month, int day)
 		{
-			this.ViewData["UserDate"] = this.BuildDate(year, month, day);
+			this.ViewData["UserDate"] = TimeUtility.BuildDate(year, month, day);
 
 			return View();
 		}
 
 		#endregion Controller Actions
-
-		#region Utility Methods
-
-		private DateTime BuildDate(int year, int month, int day)
-		{
-			DateTime now = DateTime.UtcNow;
-
-			if (year <= 0 || year >= 9999)
-			{
-				return new DateTime(now.Year, now.Month, now.Day, 12, 0, 0, DateTimeKind.Unspecified);
-			}
-
-			if (month < 1 || month > 12)
-			{
-				return new DateTime(year, now.Month, now.Day, 12, 0, 0, DateTimeKind.Unspecified);
-			}
-
-			if (day < 1 || day > 31)
-			{
-				return new DateTime(year, month, 1, 12, 0, 0, DateTimeKind.Unspecified);
-			}
-
-			return new DateTime(year, month, day, 12, 0, 0, DateTimeKind.Unspecified);
-		}
-
-		#endregion Utility Methods
 	}
 }
---
+++ CalendarApp/MvcWebApp/Models/TimeUtility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web;
+
+namespace MvcWebApp.Models
+{
+	public class TimeUtility
+	{
+		#region Methods
+
+		public static DateTime BuildDate(int year, int month, int day)
+		{
+			if (year <= 0 || year >= 9999)
+			{
+				return DateTime.UtcNow;
+			}
+
+			if (month < 1 || month > 12)
+			{
+				// unspecified is browser time zone
+				return new DateTime(year, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
+			}
+
+			if (day < 1 || day > 31)
+			{
+				// unspecified is browser time zone
+				return new DateTime(year, month, 1, 0, 0, 0, DateTimeKind.Unspecified);
+			}
+
+			// unspecified is browser time zone
+			return new DateTime(year, month, day, 0, 0, 0, DateTimeKind.Unspecified);
+		}
+
+		/// <summary>
+		/// Establishes the user's timezone for this request
+		/// </summary>
+		/// <param name="context"></param>
+		public static void SetupTimeZone(HttpContext context)
+		{
+			HttpCookie cookie = context.Request.Cookies.Get("tz");
+
+			int offset;
+			TimeZoneInfo timezone;
+			if (cookie == null || !Int32.TryParse(cookie.Value, out offset))
+			{
+				// use server time
+				timezone = TimeZoneInfo.Local;
+			}
+			else
+			{
+				// use browser time
+				timezone = TimeZoneInfo.CreateCustomTimeZone(
+					cookie.Value,
+					TimeSpan.FromMinutes(-offset),// reverse direction
+					cookie.Value,
+					cookie.Value);
+			}
+
+			context.Items["TimeZone"] = timezone;
+		}
+
+		#endregion Methods
+	}
+}
