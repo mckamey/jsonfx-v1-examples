4b181f9bca1df90fdf0cbcb709220d9897b02e7b
Stephen McKamey <mckamey@users.noreply.github.com>
2009-05-11 06:28:07 +0000

    - using Nullable<T> as a model for RichTextValue

--- CalendarApp/MvcWebApp/Models/RichTextValue.cs
+++ CalendarApp/MvcWebApp/Models/RichTextValue.cs
@@ -43,5 +43,39 @@ namespace CalendarApp.Models
 		}
 
 		#endregion Properties
+
+		#region Methods
+
+		public override bool Equals(object that)
+		{
+			if (that is RichTextValue)
+			{
+				return this.Value.Equals(((RichTextValue)that).Value);
+			}
+
+			return this.Value.Equals(that);
+		}
+
+		public override int GetHashCode()
+		{
+			return this.Value.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return this.Value.ToString();
+		}
+
+		public static implicit operator RichTextValue(string value)
+		{
+			return new RichTextValue(value);
+		}
+
+		public static explicit operator string(RichTextValue value)
+		{
+			return value.Value;
+		}
+
+		#endregion Methods
 	}
 }
