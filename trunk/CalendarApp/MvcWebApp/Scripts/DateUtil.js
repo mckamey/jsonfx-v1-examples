1feef1ef98f878a53e061f01bedb93e112fc6f4e
smm <smm@32305b5c-24dc-11de-a8df-a7b4aff12ff9>
2009-05-20 04:34:45 +0000

    - moving date filtering logic into shared util

---
+++ trunk/CalendarApp/MvcWebApp/Scripts/DateUtil.js
@@ -0,0 +1,33 @@
+﻿/*global Calendar */
+
+/* namespace window.Calendar */
+if ("undefined" === typeof window.Calendar) {
+	window.Calendar = {};
+}
+
+Calendar.DateUtil = {
+
+	filterRange : function(/*Events[]*/ events, /*Date*/ start, /*Date*/ end) {
+		var items = [];
+
+		for (var i=0; i<events.length; i++) {
+			if (events[i].Starting.between(start, end) ||
+				events[i].Ending.between(start, end)) {
+				items.push(events[i]);
+			}
+		}
+
+		return items;
+	},
+
+	filterDay : function(/*Events[]*/ events, /*Date*/ date) {
+		var start = date.clone().clearTime(),
+			end = date.clone().set( {
+				hour: 23,
+				minute: 59,
+				second: 59
+			});
+
+		return Calendar.DateUtil.filterRange(events, start, end);
+	}
+};
