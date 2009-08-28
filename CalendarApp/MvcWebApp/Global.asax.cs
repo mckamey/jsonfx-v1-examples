41cc633f73285a6c01aa86b6aa6aebcf10ff983d
Stephen McKamey <mckamey@users.noreply.github.com>
2009-08-28 15:32:13 +0000

    - adding ability for app to auto-gen the DB on first run

--- CalendarApp/MvcWebApp/Global.asax.cs
+++ CalendarApp/MvcWebApp/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web;
 
 using CalendarApp.Models;
 
@@ -9,6 +11,40 @@ namespace CalendarApp
 {
 	public class MvcApplication : System.Web.HttpApplication
 	{
+		private void EnsureDatabase()
+		{
+			string connection = ConfigurationManager.ConnectionStrings["CalendarConnectionString"].ConnectionString;
+			if (connection != null && connection.IndexOf("|DataDirectory|") >= 0)
+			{
+				string dataDir = HostingEnvironment.MapPath("~/App_Data");
+				if (!Directory.Exists(dataDir))
+				{
+					Directory.CreateDirectory(dataDir);
+				}
+				connection = connection.Replace("|DataDirectory|", dataDir);
+			}
+
+			CalendarDataContext db = new CalendarDataContext(connection);
+			if (db.DatabaseExists())
+			{
+				return;
+			}
+
+			// create the DB
+			db.CreateDatabase();
+
+			// add a system user
+			Person sysadmin = new Person();
+			sysadmin.OpenID = "http://example.com/openid";
+			sysadmin.FirstName = "System";
+			sysadmin.LastName = "Admin";
+			sysadmin.CreatedDate = DateTime.UtcNow;
+			sysadmin.CreatedBy = 1L;// created self
+			db.Persons.InsertOnSubmit(sysadmin);
+
+			db.SubmitChanges();
+		}
+
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			//http://msdn.microsoft.com/en-us/magazine/2009.01.extremeaspnet.aspx
@@ -82,6 +118,7 @@ namespace CalendarApp
 		protected void Application_Start()
 		{
 			RegisterRoutes(RouteTable.Routes);
+			EnsureDatabase();
 		}
 	}
 }
