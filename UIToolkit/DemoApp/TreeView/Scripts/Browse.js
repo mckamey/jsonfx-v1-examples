1706a0ef2c99ab2cf9a93cb1c229b8d3e0cadde5
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-13 16:53:28 +0000

    - overhaul of tree view - moving all key events into control / library - moving preview loading helpers directly into controls - removing behavior bindings since can attach events directly during databinding - fixed extra whitespace on preview - added key command to close preview

--- UIToolkit/DemoApp/TreeView/Scripts/Browse.js
+++ UIToolkit/DemoApp/TreeView/Scripts/Browse.js
@@ -1,130 +1,141 @@
 ﻿/*global JsonFx, JbstUIToolkit, window */
 
-/* namespace DemoApp */
+/* namespace DemoApp.TreeView */
 var DemoApp;
 if ("undefined" === typeof DemoApp) {
 	DemoApp = {};
 }
+if ("undefined" === typeof DemoApp.TreeView) {
+	DemoApp.TreeView = {};
+}
 
 /*-------------------------------------------------------------------*/
 
-/*void*/ DemoApp.loadComplete = function(/*object*/ data, /*object*/ cx) {
-	if (!data) {
-		return;
-	}
-
-	var elem = cx && cx.elem;
-	if (elem && data.category === "Folder") {
-		// lazy loaded data is a sub tree
-		elem.onclick = function(/*Event*/ evt) {
-			JbstUIToolkit.TreeView.toggle(elem);
-			return false;
-		};
-
-		return JbstUIToolkit.TreeView.addSubTree(elem, data);
-	}
-};
-
-/*void*/ DemoApp.loadError = function (/*object*/ result, /*object*/ cx, /*Error*/ ex) {
-	var msg = ex.message || ex.description || ex;
-	if (msg && window.confirm(msg)) {
-		/*jslint debug:true */
-		debugger;
-		/*jslint debug:false */
-	}
-};
-
-/*const string*/ DemoApp.host = (window.location.protocol+"//"+window.location.host);
-
-/*void*/ DemoApp.lazyLoad = function(/*DOM*/ elem) {
-	if (!elem) {
-		return;
-	}
-
-	// begin perf timing
-//	var start = Perf.now();
-
-	var path = elem.href||"";
-	if (path.indexOf(DemoApp.host) === 0) {
-		// DOM hrefs get fully qualified
-		path = path.substr(DemoApp.host.length);
-	}
-
-	DemoApp.BrowseService.browse(
-		path,
-		{
-			onSuccess : DemoApp.loadComplete,
-			onFailure : DemoApp.loadError,
-			onComplete : function(/*XHR*/ r, /*object*/ cx) {
-//				Perf.add(Perf.now() - start);
-			},
-			context : { elem: elem }
-		});
-};
-
-/*void*/ DemoApp.loadPreview = function(/*DOM*/ elem) {
-	if (!elem) {
-		return;
-	}
-
-	// begin perf timing
-//	var start = Perf.now();
-
-	var path = elem.href;
-	if (path.indexOf(DemoApp.host) === 0) {
-		// DOM hrefs get fully qualified
-		path = path.substr(DemoApp.host.length);
-	}
-
-	DemoApp.BrowseService.view(
-		path,
-		{
-			onSuccess : DemoApp.previewFile,
-			onFailure : DemoApp.loadError,
-			onComplete : function(/*XHR*/ r, /*object*/ cx) {
-//				Perf.add(Perf.now() - start);
-			},
-			context : { elem: elem }
-		});
-};
-
+// override the control methods to provide an app-specific implementation
 (function() {
-	// override the template to provide a data specific binding
-	// create a closure containing the old method using a familiar syntax
-	// now the new method can reference the old method when overriding
-	var base = { getLabelCss: JbstUIToolkit.TreeView.TreeNode.getLabelCss };
+
+	/* override JbstUIToolkit.TreeView.TreeNode.getAction ------------------ */
+
+	/*const string*/ var host = (window.location.protocol+"//"+window.location.host);
+
+	/*void*/ function lazyLoad(/*Event*/ evt) {
+		// begin perf timing
+//		var start = Perf.now();
+
+		var path = this.href||"";
+		if (path.indexOf(host) === 0) {
+			// DOM hrefs get fully qualified
+			path = path.substr(host.length);
+		}
+
+		var elem = this;
+		DemoApp.BrowseService.browse(
+			path,
+			{
+				onSuccess : function(/*object*/ data, /*object*/ cx) {
+					if (!data || !elem) {
+						return;
+					}
+
+					if (data.category === "Folder") {
+						// lazy loaded data is a sub tree
+						elem.onclick = JbstUIToolkit.TreeView.toggle;
+
+						JbstUIToolkit.TreeView.addSubTree(elem, data);
+					}
+//				},
+//				onComplete : function(/*XHR*/ r, /*object*/ cx) {
+//					Perf.add(Perf.now() - start);
+				}
+			});
+
+		return false;
+	}
+
+	/*void*/ function loadPreview(/*Event*/ evt) {
+		// begin perf timing
+//		var start = Perf.now();
+
+		var path = this.href;
+		if (path.indexOf(host) === 0) {
+			// DOM hrefs get fully qualified
+			path = path.substr(host.length);
+		}
+
+		DemoApp.BrowseService.view(
+			path,
+			{
+				onSuccess : function(content) {
+					DemoApp.TreeView.PreviewFile.show(content);
+//				},
+//				onComplete : function(/*XHR*/ r, /*object*/ cx) {
+//					Perf.add(Perf.now() - start);
+				}
+			});
+
+		return false;
+	}
+
+	function loadImage(/*Event*/ evt) {
+		// display the image
+		DemoApp.TreeView.PreviewImage.show(this.href);
+		return false;
+	}
+
+	function openWindow(/*Event*/ evt) {
+		// open in a new window
+		window.open(this.href);
+		return false;
+	}
+
+	function voidEvent(/*Event*/ evt) {
+		// suppress event
+		return false;
+	}
+
+	// override default implementation with our custom actions
+	JbstUIToolkit.TreeView.TreeNode.getAction = function(/*object*/ data) {
+
+		// choose an action based upon category
+		switch (data && data.category) {
+			case "Folder":
+				return lazyLoad;
+
+			case "Code":
+			case "Text":
+			case "Xml":
+				return loadPreview;
+
+			case "Image":
+				return loadImage;
+
+			case "Audio":
+			case "Compressed":
+			case "Document":
+			case "Video":
+				return openWindow;
+
+			default:
+				return voidEvent;
+		}
+	};
+
+	/* override JbstUIToolkit.TreeView.TreeNode.getLabelCss ------------------ */
+
+	// create a closure containing the old method now the
+	// new method can reference the old method when overriding
+	var baseGetLabelCss = JbstUIToolkit.TreeView.TreeNode.getLabelCss;
 
 	JbstUIToolkit.TreeView.TreeNode.getLabelCss = function(/*object*/ data) {
 		// this syntax is made available via the closure
-		var css = base.getLabelCss(data);
+		var css = baseGetLabelCss(data);
 
 		if (!data.lazyLoad) {
 			return css+" Download";
 		}
 
-		// using JsonFx behavior bindings to
-		// automatically hookup appropriate actions
-		switch (data.category) {
-			case "Folder":
-				css += " LazyLoad js-LazyLoad";
-				break;
-			case "Code":
-			case "Text":
-			case "Xml":
-				css += " js-FilePreview";
-				break;
-			case "Image":
-				css += " js-ImagePreview";
-				break;
-			case "Audio":
-			case "Compressed":
-			case "Document":
-			case "Video":
-				css += " js-ExtLink";
-				break;
-			default:
-				css += " js-Void";
-				break;
+		if (data.category === "Folder") {
+			css += " LazyLoad";
 		}
 
 		css += " "+data.category+"Label";
@@ -139,71 +150,16 @@ if ("undefined" === typeof DemoApp) {
 		}
 		return css;
 	};
-})();
 
-(function() {
-	// override the template to provide a data specific binding
-	// create a closure containing the old method using a familiar syntax
-	// now the new method can reference the old method when overriding
-	var base = { getChildren: JbstUIToolkit.TreeView.TreeRoot.getChildren };
-
+	/* override JbstUIToolkit.TreeView.TreeRoot.getChildren ------------------ */
+	
+	// override default implementation
 	JbstUIToolkit.TreeView.TreeRoot.getChildren = function(/*object*/ data) {
-		if (!data) {
-			// this syntax is made available via the closure
-			return base.getChildren(data);
+		if (!data || !data.children) {
+			return [];
 		}
 
 		return data.children;
 	};
+
 })();
-
-JsonFx.Bindings.add(
-	"a.js-LazyLoad",
-	function(/*DOM*/ elem) {
-		elem.onclick = function(/*Event*/ evt) {
-			DemoApp.lazyLoad(elem);
-			return false;
-		};
-	},
-	function(/*DOM*/ elem) {
-		elem.data = null;
-		elem.onclick = null;
-	});
-
-JsonFx.Bindings.add(
-	"a.js-FilePreview",
-	function(/*DOM*/ elem) {
-		elem.onclick = function(/*Event*/ evt) {
-			DemoApp.loadPreview(elem);
-			return false;
-		};
-	},
-	function(/*DOM*/ elem) {
-		elem.data = null;
-		elem.onclick = null;
-	});
-
-JsonFx.Bindings.add(
-	"a.js-ImagePreview",
-	function(/*DOM*/ elem) {
-		elem.onclick = function(/*Event*/ evt) {
-			DemoApp.previewImage(elem.href);
-			return false;
-		};
-	},
-	function(/*DOM*/ elem) {
-		elem.data = null;
-		elem.onclick = null;
-	});
-
-JsonFx.Bindings.add(
-	"a.js-Void",
-	function(/*DOM*/ elem) {
-		elem.onclick = function(/*Event*/ evt) {
-			return false;
-		};
-	},
-	function(/*DOM*/ elem) {
-		elem.data = null;
-		elem.onclick = null;
-	});
--- UIToolkit/JsonFx.UIToolkit/TreeView/TreeView.js
+++ UIToolkit/JsonFx.UIToolkit/TreeView/TreeView.js
@@ -20,6 +20,8 @@ if ("undefined" === typeof JbstUIToolkit.TreeView) {
 	JbstUIToolkit.TreeView = {};
 }
 
+/* public methods ----------------------------------------------*/
+
 /*void*/ JbstUIToolkit.TreeView.addSubTree = function(/*DOM*/ elem, /*object*/ data) {
 	if (!data || !elem) {
 		return;
@@ -37,7 +39,7 @@ if ("undefined" === typeof JbstUIToolkit.TreeView) {
 	elem.parentNode.insertBefore(tree, elem.nextSibling);
 
 	// show the children
-	JbstUIToolkit.TreeView.expand(elem);
+	JbstUIToolkit.TreeView.expand.call(elem);
 };
 
 /*bool*/ JbstUIToolkit.TreeView.isCollapsed = function(/*DOM*/ elem) {
@@ -45,167 +47,27 @@ if ("undefined" === typeof JbstUIToolkit.TreeView) {
 	return JsonFx.UI.hasClass(elem, "js-ClosedNode");
 };
 
-/*void*/ JbstUIToolkit.TreeView.expand = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
+/*void*/ JbstUIToolkit.TreeView.expand = function(/*Event*/ evt) {
+	var elem = JsonFx.UI.findParent(this, "js-TreeNode");
 	JsonFx.UI.removeClass(elem, "js-ClosedNode");
+
+	return false;
 };
 
-/*void*/ JbstUIToolkit.TreeView.collapse = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
+/*void*/ JbstUIToolkit.TreeView.collapse = function(/*Event*/ evt) {
+	var elem = JsonFx.UI.findParent(this, "js-TreeNode");
 	JsonFx.UI.addClass(elem, "js-ClosedNode");
+
+	return false;
 };
 
-/*void*/ JbstUIToolkit.TreeView.toggle = function(/*DOM*/ elem) {
-	if (JbstUIToolkit.TreeView.isCollapsed(elem)) {
-		JbstUIToolkit.TreeView.expand(elem);
+/*void*/ JbstUIToolkit.TreeView.toggle = function(/*Event*/ evt) {
+	if (JbstUIToolkit.TreeView.isCollapsed(this)) {
+		JbstUIToolkit.TreeView.expand.call(this);
 	} else {
-		JbstUIToolkit.TreeView.collapse(elem);
+		JbstUIToolkit.TreeView.collapse.call(this);
 	}
-	elem.focus();
+	this.focus();
+
+	return false;
 };
-
-/*DOM*/ JbstUIToolkit.TreeView.select = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findChild(elem, "js-NodeLabel");
-	if (elem) {
-		elem.focus();
-	}
-	return elem;
-};
-
-/*DOM*/ JbstUIToolkit.TreeView.moveParent = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode", true);
-	elem = JbstUIToolkit.TreeView.select(elem);
-	return elem;
-};
-
-/*DOM*/ JbstUIToolkit.TreeView.moveChild = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
-	elem = JsonFx.UI.findChild(elem, "js-TreeNode", true);
-	elem = JbstUIToolkit.TreeView.select(elem);
-	return elem;
-};
-
-/*DOM*/ JbstUIToolkit.TreeView.movePrev = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
-	elem = JsonFx.UI.findPrev(elem, "js-TreeNode", true);
-	elem = JbstUIToolkit.TreeView.select(elem);
-	return elem;
-};
-
-/*DOM*/ JbstUIToolkit.TreeView.moveNext = function(/*DOM*/ elem) {
-	elem = JsonFx.UI.findParent(elem, "js-TreeNode");
-	elem = JsonFx.UI.findNext(elem, "js-TreeNode", true);
-	elem = JbstUIToolkit.TreeView.select(elem);
-	return elem;
-};
-
-/* Event utilities ----------------------------------------------*/
-
-/*void*/ JbstUIToolkit.TreeView.clearEvent = function(/*Event*/ evt) {
-	evt = evt || window.event;
-	if (evt) {
-		if (evt.stopPropagation) {
-			evt.stopPropagation();
-			evt.preventDefault();
-		} else {
-			try {
-				evt.cancelBubble = true;
-				evt.returnValue = false;
-			} catch (ex) {
-				// IE6
-			}
-		}
-	}
-};
-
-/*int*/ JbstUIToolkit.TreeView.getKeyCode = function(/*Event*/ evt) {
-	evt = evt || window.event;
-	if (!evt) {
-		return -1;
-	}
-	return Number(evt.keyCode || evt.charCode || -1);
-};
-
-/*void*/ JbstUIToolkit.TreeView.onkeydown = function(/*Event*/ evt, /*DOM*/ elem) {
-	evt = evt || window.event;
-
-	switch (JbstUIToolkit.TreeView.getKeyCode(evt)) {
-		case 0x0D: // enter
-		case 0x20: // space
-			if (elem.click) {
-				elem.click();
-			} else if (elem.onclick) {
-				elem.onclick();
-			}
-			break;
-
-		case 0x25: // left arrow
-			if (!JbstUIToolkit.TreeView.isCollapsed(elem)) {
-				JbstUIToolkit.TreeView.collapse(elem);
-			} else {
-				JbstUIToolkit.TreeView.moveParent(elem);
-			}
-			break;
-
-		case 0x28: // down arrow
-			if (!JbstUIToolkit.TreeView.isCollapsed(elem)) {
-				JbstUIToolkit.TreeView.moveChild(elem);
-			} else {
-				while (elem && !JbstUIToolkit.TreeView.moveNext(elem)) {
-					elem = JbstUIToolkit.TreeView.moveParent(elem);
-				}
-			}
-			break;
-
-		case 0x26: // up arrow
-			var prev = JbstUIToolkit.TreeView.movePrev(elem);
-			if (!prev) {
-				JbstUIToolkit.TreeView.moveParent(elem);
-			} else if (!JbstUIToolkit.TreeView.isCollapsed(prev)) {
-				prev = JbstUIToolkit.TreeView.moveChild(prev);
-				while (prev) {
-					prev = JbstUIToolkit.TreeView.moveNext(prev);
-				}
-			}
-			break;
-
-		case 0x27: // right arrow
-			if (JbstUIToolkit.TreeView.isCollapsed(elem)) {
-				if (elem.click) {
-					elem.click();
-				} else if (elem.onclick) {
-					elem.onclick();
-				}
-			} else {
-				JbstUIToolkit.TreeView.moveChild(elem);
-			}
-			break;
-
-		default:
-//			alert(JbstUIToolkit.TreeView.getKeyCode(evt));
-			return;
-	}
-	JbstUIToolkit.TreeView.clearEvent(evt);
-};
-
-JsonFx.Bindings.add(
-	"a.js-NodeLabel",
-	function(/*DOM*/ elem) {
-		elem.onkeydown = function(/*Event*/ evt) {
-			return JbstUIToolkit.TreeView.onkeydown(evt, elem);
-		};
-		elem.ondblclick = function(/*Event*/ evt) {
-			if (elem.click) {
-				return elem.click(evt);
-			}
-			if (elem.onclick) {
-				return elem.onclick(evt);
-			}
-		};
-	},
-	function(/*DOM*/ elem) {
-		elem.onkeydown = null;
-		elem.ondblclick = null;
-	});
-
