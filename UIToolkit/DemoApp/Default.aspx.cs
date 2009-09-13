3e43f3c4d0a3d94a87cb45467a27484163d8784e
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-13 02:32:50 +0000

    - adding project structure - adding svn:ignore

---
+++ UIToolkit/DemoApp/Default.aspx.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace JsonFx.WapTemplate
+{
+	public partial class _Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!this.Context.IsDebuggingEnabled)
+			{
+				// improve the Yslow rating
+				JsonFx.Handlers.ResourceHandler.EnableStreamCompression(this.Context);
+			}
+		}
+	}
+}
---
+++ UIToolkit/JsonFx.UIToolkit/Properties/AssemblyVersion.cs
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
+
+#region 1.1.909.1219
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.1.909.1219")]
+[assembly: AssemblyFileVersion("1.1.909.1219")]
+
+#endregion 1.1.909.1219
