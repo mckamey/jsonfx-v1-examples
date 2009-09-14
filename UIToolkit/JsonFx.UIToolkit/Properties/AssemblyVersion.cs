e4d8b497467da556afd3372b405d87c8660b3bab
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-14 15:36:17 +0000

    - fixing up project references

--- UIToolkit/JsonFx.UIToolkit/Properties/AssemblyVersion.cs
+++ UIToolkit/JsonFx.UIToolkit/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.1.909.1219
+#region 1.1.909.1408
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.909.1219")]
-[assembly: AssemblyFileVersion("1.1.909.1219")]
+[assembly: AssemblyVersion("1.1.909.1408")]
+[assembly: AssemblyFileVersion("1.1.909.1408")]
 
-#endregion 1.1.909.1219
+#endregion 1.1.909.1408
