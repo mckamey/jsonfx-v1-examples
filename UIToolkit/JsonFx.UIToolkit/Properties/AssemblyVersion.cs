a72ad75aeed724870c18582498fbea044bcf680e
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-19 19:57:19 +0000

    - rev'ing version number

--- UIToolkit/JsonFx.UIToolkit/Properties/AssemblyVersion.cs
+++ UIToolkit/JsonFx.UIToolkit/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.909.1808
+#region 1.0.909.1912
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.909.1808")]
-[assembly: AssemblyFileVersion("1.0.909.1808")]
+[assembly: AssemblyVersion("1.0.909.1912")]
+[assembly: AssemblyFileVersion("1.0.909.1912")]
 
-#endregion 1.0.909.1808
+#endregion 1.0.909.1912
